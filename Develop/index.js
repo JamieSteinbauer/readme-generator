2f0c667aa2a29458450c2513e3edad5eed9a4eb2
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-04-18 21:07:09 -0400

    created questions array

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,75 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generateMarkdown = require('./utils/generateMarkdown.js');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the title of your project? (required)",
+            validate: (titleInput) => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a title!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Enter a description about your project. (required)",
+            validate: (descriptionInput) => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a description!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "Enter the installation instructions for your project."
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Please enter the usage instructions for your project."
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "Select a license for your project",
+            choices: ["MIT", "Apache", "GPL", "BSD", "None"]
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "Enter the instructions for contributing to your project."
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "Enter your GitHub username"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email address"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "Enter the instructions for testing your project"
+        }
+    ])
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
